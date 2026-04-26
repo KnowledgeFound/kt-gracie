2d2fff8bb532d95ff6b45847f9c89f1480072548
theaman249 <theaman249@gmail.com>
2026-04-26 14:26:40 +0200

    Refactor GreetingPixiPage component to enable greeting bubbles rendering

--- src/kt-gracie-frontend/src/components/pixi/greeting/page.tsx
+++ src/kt-gracie-frontend/src/components/pixi/greeting/page.tsx
@@ -1,13 +1,31 @@
-// import { Container, Graphics, Stage } from "@pixi/react";
-// import { useGreetingBubbles } from "./hooks/useGreetingBubbles";
-
+import { Container, Graphics, Stage } from "@pixi/react";
+import { useGreetingBubbles } from "./hooks/useGreetingBubbles";
 
 export default function GreetingPixiPage() {
-  // const { bubbles, viewport } = useGreetingBubbles();
+  const { bubbles, viewport } = useGreetingBubbles();
 
   return (
-    <>
-      <div></div>
-    </>
+    <Stage
+      width={viewport.width}
+      height={viewport.height}
+      options={{
+        backgroundAlpha: 0,
+        antialias: true,
+      }}
+    >
+      <Container>
+        {bubbles.map((bubble) => (
+          <Graphics
+            key={bubble.id}
+            draw={(graphics) => {
+              graphics.clear();
+              graphics.beginFill(0x68d8ff, bubble.alpha);
+              graphics.drawCircle(bubble.x, bubble.y, bubble.radius);
+              graphics.endFill();
+            }}
+          />
+        ))}
+      </Container>
+    </Stage>
   );
 }
