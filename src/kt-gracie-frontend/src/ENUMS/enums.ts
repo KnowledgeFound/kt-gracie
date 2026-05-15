98767abe001d10586c9e54840a101c0da3e3530f
theaman249 <theaman249@gmail.com>
2026-05-15 23:37:22 +0200

    Add CityState enum and enhance City class with state management methods

--- src/kt-gracie-frontend/src/ENUMS/enums.ts
+++ src/kt-gracie-frontend/src/ENUMS/enums.ts
@@ -3,3 +3,9 @@ export enum Gender {
     FEMALE = "female",
     OTHER = "other",
 }
+
+export enum CityState {
+    VIBRANT = "Vibrant",
+    NORMAL = "Normal",
+    CORRUPT = "Corrupt",
+}
--- src/kt-gracie-frontend/src/models/City.ts
+++ src/kt-gracie-frontend/src/models/City.ts
@@ -1,21 +1,58 @@
+import { CityState } from "../ENUMS/enums";
+
 export class City {
     private name: string;
-    private health: number;
+    private health: number = 100;
+    private decay: number = 5;
+    private contentScore: number = 0;
+    private finalAssessmentScore: number = 0;
 
-    constructor() {
-        this.name = "Generic City Name";
-        this.health = 100;
+
+    constructor(name: string) {
+        this.name = name;
     }
 
     public getName(): string {
         return this.name;
     }
 
+    getContentScore(): number {
+        return this.contentScore;
+    }
+
+    setContentScore(score: number): void {
+        this.contentScore = score;
+    }
+
+    getFinalAssessmentScore(): number {
+        return this.finalAssessmentScore;
+    }
+
+    setFinalAssessmentScore(score: number): void {
+        this.finalAssessmentScore = score;
+    }
+
     public getHealth(): number {
-        return this.health;
+        return (0.5 * this.contentScore) + (0.5 * this.finalAssessmentScore) - this.decay;
     }
 
     public setHealth(health: number): void {
         this.health = health;
+    }
+
+    public decayCityHealth(): void {
+        this.health -= this.decay;
+    }
+
+    public getCityState(): CityState {
+        const health = this.getHealth();
+
+        if (health >= 60) {
+            return CityState.VIBRANT;
+        } else if (health >= 40) {
+            return CityState.NORMAL;
+        } else{
+            return CityState.CORRUPT;
+        }
     }
 }
