d3e9181e832d650164db566cb6018943eb928aa9
amanda9805 <akhuzwayo98@gmail.com>
2026-09-15 14:39:07 +0200

    Randomiser funcs

--- src/kt-gracie-frontend/src/services/gameEngine.ts
+++ src/kt-gracie-frontend/src/services/gameEngine.ts
@@ -47,7 +47,27 @@ function getFlashCards(corpus: Corpus): Card[] {
     return shuffleArray(flashCards);
 }
 
+export async function quizQuestionRandomiser(numQuestions: number): Promise<QuizQuestion[]> {
 
+    const corpus = await getCorpus();
+    const quizQuestions = getQuizQuestions(corpus);
+    const num = numQuestions > quizQuestions.length ? quizQuestions.length : numQuestions;
+
+    const shuffledQuestions = shuffleArray(quizQuestions);
+
+    return shuffledQuestions.slice(0, num) || [];
+}
+
+export async function flashCardRandomiser(numQuestions: number): Promise<Card[]> {
+    
+    const corpus = await getCorpus();
+    const flashCards = getFlashCards(corpus);
+    const num = numQuestions > flashCards.length ? flashCards.length : numQuestions;
+
+    const shuffledFlashCards = shuffleArray(flashCards);
+
+    return shuffledFlashCards.slice(0, num) || [];
+}
 
 
 
