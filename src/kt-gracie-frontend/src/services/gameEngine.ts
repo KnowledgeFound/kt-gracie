fb139b0bf5a9ca51a75041eca38e0c25499a3109
amanda9805 <akhuzwayo98@gmail.com>
2026-09-15 14:36:59 +0200

    Helper funcs

---
+++ src/kt-gracie-frontend/src/services/gameEngine.ts
@@ -0,0 +1,53 @@
+
+import { getLocalStorage, setLocalStorage } from "../commons/utilts";
+import { ProgressContainer, Progress, Achievement, SubProgress } from "@/types/user";
+import { getCorpus } from "./corpusService";
+import { Corpus, Quiz, FlashCard, QuizQuestion, Card } from "@/types/types";
+import { getUser } from "./userServices";
+import { AssessmentType, Difficulty, SourceType } from "../ENUMS/enums";
+
+/**
+ * Shuffles an array of objects using the Fisher-Yates algorithm.
+ * Returns a new array to avoid mutating the original.
+ */
+function shuffleArray<T>(array: T[]): T[] {
+  const shuffled = [...array];
+
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+
+  return shuffled;
+}
+
+function getQuizQuestions(corpus: Corpus): QuizQuestion[] {
+    
+    const quizQuestions: QuizQuestion[] = [];
+    
+    if(!corpus) 
+        return [];
+    else if (corpus.knowledgeUnits[0].assessments[0].quiz?.questions){
+        quizQuestions.push(...corpus.knowledgeUnits[0].assessments[0].quiz.questions);
+    }
+
+    return shuffleArray(quizQuestions);
+}
+
+function getFlashCards(corpus: Corpus): Card[] {
+    const flashCards: Card[] = [];
+
+    if (!corpus) {
+        return [];
+    } else if (corpus.knowledgeUnits[0].assessments[0].flashcard?.cards) {
+        flashCards.push(...corpus.knowledgeUnits[0].assessments[0].flashcard.cards);
+    }
+
+    return shuffleArray(flashCards);
+}
+
+
+
+
+
